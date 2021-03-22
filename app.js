9371a56cb4d0555489c6685cee84d4b3ea06a98c
Andrew Choi <choiaw1991@gmail.com>
2021-03-21 22:05:14 -0700

    Added more text

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World! This is my DevOps Project Number 1!'))
+app.get('/', (req, res) => res.send('Hello World! This is my DevOps Project Number 1! Added more text!'))
 app.listen(3000, () => console.log('Server ready'))
